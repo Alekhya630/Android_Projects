dd5491c01f8b0eb5670a628373eeffa0e0332e15
Alekhya630 <alekhyaperam9634@gmail.com>
2024-12-19 14:50:22 +0530

    Delete HerosDetails directory

--- HerosDetails/app/src/main/java/com/singlepointsol/herosdetails/Heros.kt
+++
@@ -1,8 +0,0 @@
-package com.singlepointsol.herosdetails
-
-data class Heros(
-    var bornyear: String? = null,
-    var actor: String? = null,
-    var education: String? = null,
-    var nickname: String? = null
-)
