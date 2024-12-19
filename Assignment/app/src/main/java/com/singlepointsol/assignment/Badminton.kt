11dbbb88277a5a079281dbf782d7e84bab3ab5e0
Alekhya630 <alekhyaperam9634@gmail.com>
2024-12-19 14:38:25 +0530

    Delete Assignment directory

--- Assignment/app/src/main/java/com/singlepointsol/assignment/Badminton.kt
+++
@@ -1,4 +0,0 @@
-package com.singlepointsol.assignment
-
-
-data class Badminton(val name: String?=null, val country: String?=null, val year: String?=null, val handedness: String?=null, val coachName: String?=null, val matches: String?=null)
